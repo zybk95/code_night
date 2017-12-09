79e2f55d117b95c208123b848b92dccb0874dbc3
Abdülkadir ZEYBEK <abdulkadirzeybek95@gmail.com>
2017-12-09 05:58:43 +0300

    update ozelligi eklenir

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,20 @@
 class MoviesController < ApplicationController
+
+
+
   def index
     @movies = Movie.all
   end
 
+
+
+
   def new
     @movie = Movie.new
   end
+
+
+
 
   def create
     @movie=Movie.new(movie_params)
@@ -19,12 +28,42 @@ class MoviesController < ApplicationController
 
   end
 
+
+
+
+  def edit
+    @movie=Movie.find(params[:id])
+  end
+
+
+
+
+
+  def update
+    @movie=Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+
+
+
   def show
     @movie=Movie.find(params[:id])
   end
+
+
+
 
   private #private nin altına girilen herşey privatedir...
   def movie_params
     params.require(:movie).permit(:name, :description, :avatar)
   end
+
+
+
 end
